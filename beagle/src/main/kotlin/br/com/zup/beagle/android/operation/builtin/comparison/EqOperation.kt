1d5f51f6a19c9e4ed41f11ebe04eeef3be42030b
unknown <tomerpacific@gmail.com>
2022-10-22 10:54:06 +0300

    feature/eq-not-working using newly created interface with EqOperation

--- beagle/src/main/kotlin/br/com/zup/beagle/android/operation/builtin/comparison/EqOperation.kt
+++ beagle/src/main/kotlin/br/com/zup/beagle/android/operation/builtin/comparison/EqOperation.kt
@@ -19,10 +19,9 @@ package br.com.zup.beagle.android.operation.builtin.comparison
 import br.com.zup.beagle.android.operation.Operation
 import br.com.zup.beagle.android.operation.OperationType
 import br.com.zup.beagle.android.annotation.RegisterOperation
-import java.lang.NumberFormatException
 
 @RegisterOperation("eq")
-internal class EqOperation : Operation {
+internal class EqOperation : Operation, ComparisonOperationHandler {
 
     override fun execute(vararg params: OperationType?): OperationType {
         return when (params.size) {
@@ -33,44 +32,14 @@ internal class EqOperation : Operation {
 
     private fun handleOperation(first: OperationType?, second: OperationType?): OperationType {
         val result = when {
-            first?.value is Int && second?.value is String -> handleStringAndIntCase(first, second)
-            second?.value is String && first?.value is Int -> handleStringAndIntCase(second, first)
-            first?.value is Double && second?.value is String -> handleStringAndDoubleCase(first, second)
-            second?.value is String && first?.value is Int -> handleStringAndDoubleCase(second, first)
-            first?.value is Int && second?.value is Double -> handleIntAndDoubleCase(first, second)
-            second?.value is Int && first?.value is Double -> handleIntAndDoubleCase(second, first)
+            first?.value is Int && second?.value is String -> handleStringAndIntCase(first, second, this)
+            second?.value is String && first?.value is Int -> handleStringAndIntCase(second, first, this)
+            first?.value is Double && second?.value is String -> handleStringAndDoubleCase(first, second, this)
+            second?.value is String && first?.value is Int -> handleStringAndDoubleCase(second, first, this)
+            first?.value is Int && second?.value is Double -> handleIntAndDoubleCase(first, second, this)
+            second?.value is Int && first?.value is Double -> handleIntAndDoubleCase(second, first, this)
             else -> false
         }
         return OperationType.TypeBoolean(result)
     }
-
-    private fun handleStringAndIntCase(first: OperationType?, second: OperationType?): Boolean {
-        try {
-            val secondAsInt: Int = (second?.value as String).toInt()
-            return (first?.value as Int) == secondAsInt
-        } catch (exception: NumberFormatException) {
-
-        }
-        return false
-    }
-
-    private fun handleStringAndDoubleCase(first: OperationType?, second: OperationType?): Boolean {
-        try {
-            val secondAsDouble: Double = (second?.value as String).toDouble()
-            return (first?.value as Double) == secondAsDouble
-        } catch (exception: NumberFormatException) {
-
-        }
-        return false
-    }
-
-    private fun handleIntAndDoubleCase(first: OperationType?, second: OperationType?): Boolean {
-        val secondAsDouble: Double = second?.value as Double
-        val integerPartOfDouble: Int = secondAsDouble.toInt()
-        return when (secondAsDouble - integerPartOfDouble) {
-            0.0 -> (first?.value as Int) == integerPartOfDouble
-            else -> false
-        }
-    }
-
 }
