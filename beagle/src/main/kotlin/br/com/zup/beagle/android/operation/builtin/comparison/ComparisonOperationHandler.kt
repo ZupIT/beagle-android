cd8c67f6e8df2d3d80127dd22724806a5984e6f9
unknown <tomerpacific@gmail.com>
2022-10-22 10:53:43 +0300

    feature/eq-not-working creating interface to handle different cases of arguments with comparison operators

---
+++ beagle/src/main/kotlin/br/com/zup/beagle/android/operation/builtin/comparison/ComparisonOperationHandler.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.android.operation.builtin.comparison
+
+import br.com.zup.beagle.android.operation.Operation
+import br.com.zup.beagle.android.operation.OperationType
+import java.lang.NumberFormatException
+
+internal interface ComparisonOperationHandler {
+
+    fun handleStringAndIntCase(first: OperationType?, second: OperationType?, operation: Operation): Boolean {
+        try {
+            val secondAsInt: Int = (second?.value as String).toInt()
+            return when (operation) {
+                is EqOperation -> (first?.value as Int) == secondAsInt
+                is GtOperation -> (first?.value as Int) > secondAsInt
+                is LtOperation -> (first?.value as Int) < secondAsInt
+                is GteOperation -> (first?.value as Int) >= secondAsInt
+                is LteOperation -> (first?.value as Int) <= secondAsInt
+                else -> false
+            }
+        } catch (exception: NumberFormatException) { }
+        return false
+    }
+
+    fun handleStringAndDoubleCase(first: OperationType?, second: OperationType?, operation: Operation): Boolean {
+        try {
+            val secondAsDouble: Double = (second?.value as String).toDouble()
+            return when (operation) {
+                is EqOperation -> (first?.value as Double) == secondAsDouble
+                is GtOperation -> (first?.value as Double) > secondAsDouble
+                is LtOperation -> (first?.value as Double) < secondAsDouble
+                is GteOperation -> (first?.value as Double) >= secondAsDouble
+                is LteOperation -> (first?.value as Double) <= secondAsDouble
+                else -> false
+            }
+        } catch (exception: NumberFormatException) { }
+        return false
+    }
+
+    fun handleIntAndDoubleCase(first: OperationType?, second: OperationType?, operation: Operation): Boolean {
+        val secondAsDouble: Double = second?.value as Double
+        val integerPartOfDouble: Int = secondAsDouble.toInt()
+
+        val decimalValue: Double = secondAsDouble - integerPartOfDouble
+
+        if (decimalValue != 0.0) {
+            return false
+        }
+
+        return when (operation) {
+            is EqOperation -> (first?.value as Int) == integerPartOfDouble
+            is GtOperation -> (first?.value as Int) > integerPartOfDouble
+            is LtOperation -> (first?.value as Int) < integerPartOfDouble
+            is GteOperation -> (first?.value as Int) >= integerPartOfDouble
+            is LteOperation -> (first?.value as Int) <= integerPartOfDouble
+            else -> false
+        }
+    }
+}
